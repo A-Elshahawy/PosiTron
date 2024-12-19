969e5fe5d7cfacbd2dd565f3305afeec806a20ba
Ahmed Elshahawy <ahmedelshahawy078@gmail.com>
2024-12-19 23:12:35 +0200

    Added README file

--- PosiTron/main.py
+++ PosiTron/main.py
@@ -1,16 +1,12 @@
-import random
-import sys
-
 import torch
 from layers.utils import subsequent_mask
-
-from .models import Bumblebee, Megatron, OptimusPrime, Starscream
-
-sys.path.append(r"D:\Projects\transformer")
+from ModelFactory import create_transformer
+from models import Bumblebee, Megatron, OptimusPrime, Starscream
 
 
 def test_inference():
     test_model = Starscream()
+    #  ? OR use ==> test_model = create_transformer(pe_type="absolute", src_vocab_size=12, tgt_vocab_size=12)
     test_model.eval()
     src = torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]])
     src_mask = torch.ones(1, 1, src.size(1))
@@ -29,21 +25,12 @@ def test_inference():
             [ys, torch.empty(1, 1).type_as(src.data).fill_(next_word)], dim=1
         )
     name = f"\033[1;32m\033[0;33m{test_model.__class__.__name__.capitalize()}\033[1;32m Transformer inference!\033[0m"
-    return name, f":: Example Untrained Model Prediction: {ys.squeeze().tolist()}"
+    return name, f":: \n \t Example Untrained Model Prediction: {ys.squeeze().tolist()}"
 
 
 def main():
-    pe_type = ["absolute", "relative", "rotary", "alibi"]
-    for _ in range(len(pe_type)):
-        p = random.choice(pe_type)
-        print(
-            " " * 20
-            + f"\033[1;32mTesting \033[0;33m{p.capitalize()}\033[1;32m Transformer inference!\033[0m"
-        )
-        for _ in range(10):
-            print(test_inference())
+    print(*test_inference())
 
 
 if __name__ == "__main__":
-    # main()
-    print(*test_inference())
+    main()
