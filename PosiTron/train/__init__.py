28a19dafde5cb292f3d3f0dff490b0ee0a3eea5d
Ahmed Elshahawy <ahmedelshahawy078@gmail.com>
2025-01-02 20:22:44 +0200

    better modules naming

--- PosiTron/train/__init__.py
+++ PosiTron/train/__init__.py
@@ -1,4 +1,14 @@
+from .inference import GreedyDecoder
+from .loss import LabelSmoothing, LossCompute
 from .trainer import Trainer, TrainingState
-from .utils import Batch, LabelSmoothing, lr_scheduler
+from .utils import Batch, lr_scheduler
 
-__all__ = ["Trainer", "TrainingState", "Batch", "lr_scheduler", "LabelSmoothing"]
+__all__ = [
+    "Trainer",
+    "TrainingState",
+    "Batch",
+    "lr_scheduler",
+    "LabelSmoothing",
+    "LossCompute",
+    "GreedyDecoder",
+]
